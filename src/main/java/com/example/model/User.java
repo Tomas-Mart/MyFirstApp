8fd449acde30360ed85b493d8b94cfae0b210ca2
Tomas-Mart <amina.tomas.mart@icloud.com>
2025-03-15 01:13:28 +0400

    В классе User добавила аннотации валидации и переопределила методы equals, hashCode и toString

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -1,6 +1,8 @@
 package com.example.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
@@ -10,11 +12,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is mandatory")
+    @Pattern(regexp = "^[a-zA-Zа-яА-Я\\s]+$", message = "Name should contain only letters")
     @Column(name = "name")
     private String name;
 
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     @Column(name = "email")
     private String email;
+
+    @Min(value = 1, message = "Age must be greater than 0")
+    @Column(name = "age")
+    private int age;
 
     // Геттеры и сеттеры
     public Long getId() {
@@ -39,5 +49,36 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
