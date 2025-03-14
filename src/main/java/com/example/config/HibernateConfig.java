6a0f57ee487896116f046d890e5fa87b5f3d3c91
Tomas-Mart <amina.tomas.mart@icloud.com>
2025-03-15 01:08:55 +0400

    В HibernateConfig подтянула свойства из файла application.properties

--- src/main/java/com/example/config/HibernateConfig.java
+++ src/main/java/com/example/config/HibernateConfig.java
@@ -14,7 +14,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:db.properties")
 public class HibernateConfig {
 
     @Value("${db.driver}")
