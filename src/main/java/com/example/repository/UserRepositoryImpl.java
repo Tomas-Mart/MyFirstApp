aed23863713cc70fadf862b159b71d2e0a8ff1a4
Tomas-Mart <amina.tomas.mart@icloud.com>
2025-03-19 14:30:35 +0400

    Убрала аннатацию @Transactional из класса UserRepositoryImpl

--- src/main/java/com/example/repository/UserRepositoryImpl.java
+++ src/main/java/com/example/repository/UserRepositoryImpl.java
@@ -2,7 +2,6 @@ package com.example.repository;
 
 import com.example.model.User;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -18,14 +17,12 @@ public class UserRepositoryImpl implements UserRepository {
     private EntityManager entityManager;
 
     @Override
-    @Transactional(readOnly = true)
     public List<User> getAllUsers() {
         logger.info("Запрос на получение списка всех пользователей из базы данных");
         return entityManager.createQuery("FROM User", User.class).getResultList();
     }
 
     @Override
-    @Transactional(readOnly = true)
     public User getUserById(Long id) {
         logger.info("Запрос на получение пользователя по ID: {}", id);
         User user = entityManager.find(User.class, id);
@@ -37,7 +34,6 @@ public class UserRepositoryImpl implements UserRepository {
     }
 
     @Override
-    @Transactional
     public void saveUser(User user) {
         logger.info("Запрос на сохранение нового пользователя: {}", user);
         entityManager.persist(user);
@@ -45,7 +41,6 @@ public class UserRepositoryImpl implements UserRepository {
     }
 
     @Override
-    @Transactional
     public void updateUser(User user) {
         logger.info("Запрос на обновление пользователя с ID {}: {}", user.getId(), user);
         entityManager.merge(user);
@@ -53,7 +48,6 @@ public class UserRepositoryImpl implements UserRepository {
     }
 
     @Override
-    @Transactional
     public void deleteUser(Long id) {
         logger.info("Запрос на удаление пользователя с ID: {}", id);
         User user = entityManager.find(User.class, id);
